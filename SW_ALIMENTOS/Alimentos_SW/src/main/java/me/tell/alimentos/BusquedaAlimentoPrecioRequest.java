a3ba8f8b182b363b142fb6e93835528dcf6d5193
Javier145 <aguilarjavier145@gmail.com>
2021-06-03 12:14:03 -0500

    Cambios sjshshs

--- SW_ALIMENTOS/Alimentos_SW/src/main/java/me/tell/alimentos/BusquedaAlimentoPrecioRequest.java
+++ SW_ALIMENTOS/Alimentos_SW/src/main/java/me/tell/alimentos/BusquedaAlimentoPrecioRequest.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.04.30 a las 02:31:51 PM CDT 
+// Generado el: 2021.06.03 a las 12:12:59 PM CDT 
 //
 
 
